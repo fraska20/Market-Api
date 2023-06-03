3f170e6365a7a28f721c3d2147e1a4e4b4a806e0
Fraska <58796786+fraska20@users.noreply.github.com>
2023-06-03 16:30:15 +0700

    inisial

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import productsRoutes from './routes/products.routes.js';
 import indexRoutes from './routes/index.routes.js';
+import bodyParser from "body-parser";
+
+//json parser
+var jsonParser = bodyParser.json();
 
 const app = express();
 
 app.use(express.json());
 
 app.use(indexRoutes);
-app.use('/api/products', productsRoutes);
+app.use('/api/products', jsonParser, productsRoutes);
 
 app.use((req, res, next) => {
     res.status(404).json({
--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ import { config } from 'dotenv';
 config()
 
 export const PORT = process.env.SERVER_PORT
-export const DB_USER = process.env.USER
-export const DB_PASSWORD = process.env.PASSWORD
-export const DB_HOST = process.env.HOST
-export const DB_DATABASE = process.env.DB
+export const DB_USER = process.env.DB_USER
+export const DB_PASSWORD = process.env.DB_PASSWORD
+export const DB_HOST = process.env.DB_HOST
+export const DB_DATABASE = process.env.DB_DATABASE
 export const DB_PORT= 3306
